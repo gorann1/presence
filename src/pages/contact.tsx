f63687e015d9bf2cbb594bccc91d466d93fca863
gorann1 <gorandevop@outlook.com>
2023-02-07 23:43:31 +0100

    #new_link

---
+++ src/pages/contact.tsx
@@ -0,0 +1,11 @@
+import styles from '@/styles/Contact.module.css';
+
+export default function Contact() {
+  return (
+    <>
+      <main className={styles.main}>
+        <h3>ContactPage</h3>
+      </main>
+    </>
+  );
+}
